1f155beb9345e487c37c9d352c7e17c3b5ec36c6
agustinaa235 <asegura@fi.uba.ar>
2022-06-01 20:14:52 -0300

    feat: agrego upload y download del cliente para st

--- src/download.py
+++ src/download.py
@@ -2,6 +2,8 @@ import logging
 import socket
 from common.parser import parse_client_download_arguments
 from client.client_download import download_file
+from selective_repeat.receiver import Receiver
+from selective_repeat.handshake import Handshake
 
 def main():
     args = parse_client_download_arguments()
@@ -19,7 +21,12 @@ def main():
     print(args)
     #RUN CLIENT
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    download_file(client ,args.name,args.dst ,addr)
+    #download_file(client ,args.name,args.dst ,addr)
+    reciver = Receiver(addr, args.dts, args.name, client)
+    handshake = Handshake('U', args.name, client, addr)
+    handshake.init_handshake()
+    reciver.start_receiver_stop_and_wait()
+    client.close()
 
 if __name__ == "__main__":
     main()
--- src/upload.py
+++ src/upload.py
@@ -1,7 +1,9 @@
 import logging
 import socket
 from common.parser import parse_client_upload_arguments
-from client.client_upload import upload_file
+#from client.client_upload import upload_file
+from selective_repeat.sender import Sender
+from selective_repeat.handshake import Handshake
 
 def main():
     args = parse_client_upload_arguments()
@@ -13,12 +15,15 @@ def main():
         log_level = logging.WARNING
 
     logging.basicConfig(level=log_level, format="%(message)s")
-    logger = logging.getLogger(__name__)
 
     print(args)
     # RUN CLIENT
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    result = upload_file(client, addr, args.src, args.name, logger)
+    #result = upload_file(client, addr, args.src, args.name, logger)
+    sender = Sender(addr, args.src, args.name, client)
+    handshake = Handshake('U', args.name, client, addr)
+    handshake.init_handshake()
+    sender.start_sender_slow_start()
     client.close()
 
 if __name__ == "__main__":
